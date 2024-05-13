c5612ab40ca65f17d9a6ec283f1378cacd6333c0
Kamal Hamdani <meripop1@gmail.com>
2024-05-12 22:01:06 -0500

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,25 @@
+using System.Text;
 using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConfiguration"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServics(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200") );
-// app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
 
-// app.UseAuthorization();
-
+    app.UseAuthentication();
+    app.UseAuthorization();
 
 app.MapControllers();
 
